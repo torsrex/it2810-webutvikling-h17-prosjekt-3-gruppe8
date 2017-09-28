e403e2ef5f878ab647df2b9e48887a5a8f22c4b3
Torstein Molland <rex.datamaskin@gmail.com>
2017-09-28 14:20:31 +0200

    :snowflake: added react-router, implemented basic routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './index.css';
 
 import Welcome from './components/Welcome'
@@ -6,48 +7,24 @@ import Calendar from './components/Calendar'
 import Notes from './components/Notes'
 import Todo from './components/Todo'
 
-export default class App extends Component {
-  constructor() {
-    super()
 
-    this.state = {
-      openedMenuItem: "welcome"
-    }
-  }
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/todo">Todo</Link></li>
+        <li><Link to="/notes">Notes</Link></li>
+        <li><Link to="/calendar">Calendar</Link></li>
+      </ul>
 
-  handleMenuItemClick(openedMenuItem) {
-    this.setState({openedMenuItem})
-  }
+      <hr/>
 
-  render() {
-    const {openedMenuItem} = this.state
-    return (
-      <div className="App">
-        <ul>
-          <li onClick={() => this.handleMenuItemClick("welcome")}>
-            Home
-          </li>
-          <li onClick={() => this.handleMenuItemClick("calendar")}>
-            Calendar
-          </li>
-          <li onClick={() => this.handleMenuItemClick("todo")}>
-            Todo
-          </li>
-          <li onClick={() => this.handleMenuItemClick("notes")}>
-            Notes
-          </li>
-        </ul>
-        {{
-          "calendar":
-          <Calendar/>,
-          "todo":
-          <Todo/>,
-          "notes":
-          <Notes/>,
-          "welcome":
-          <Welcome/>
-        }[openedMenuItem]}
-      </div>
-    )
-  }
-}
+      <Route exact path="/" component={Welcome}/>
+      <Route path="/todo" component={Todo}/>
+      <Route path="/notes" component={Notes}/>
+      <Route path="/calendar" component={Calendar}/>
+    </div>
+  </Router>
+)
+export default App
