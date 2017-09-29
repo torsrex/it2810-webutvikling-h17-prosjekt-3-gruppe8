f8d58b33dfb029cfab9d0a7ff70da3a783484adc
Torstein Molland <rex.datamaskin@gmail.com>
2017-09-29 15:12:52 +0200

    :art: :lipstick: Added react-bootstrap, switched to bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './index.css';
+import {NavItem, Nav, Navbar} from 'react-bootstrap';
 
 import Welcome from './components/Welcome'
 import Calendar from './components/Calendar'
 import Notes from './components/Notes'
 import Todo from './components/Todo'
 
-
 const App = () => (
   <Router>
     <div>
-      <header>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/todo">Todo</Link></li>
-            <li><Link to="/notes">Notes</Link></li>
-            <li><Link to="/calendar">Calendar</Link></li>
-          </ul>
-
-        </nav>
-      </header>
+      <Navbar>
+        <Nav className="navbar">
+          <NavItem eventKey={1}>
+            <Link to="/">Home</Link>
+          </NavItem>
+          <NavItem eventKey={2}>
+            <Link to="/todo">Todo</Link>
+          </NavItem>
+          <NavItem eventKey={3}>
+            <Link to="/notes">Notes</Link>
+          </NavItem>
+          <NavItem eventKey={4}>
+            <Link to="/calendar">Calendar</Link>
+          </NavItem>
+        </Nav>
+      </Navbar>
 
       <hr/>
 
