0873f588bbacf87de44e54620f568ef9dbcda357
Balázs <info@balazsorban.com>
2017-10-14 17:17:01 +0200

    :umbrella: Add tests to Calendar component.

--- react/src/__tests__/components/Calendar/index.js
+++ react/src/__tests__/components/Calendar/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Calendar from '../../../components/Calendar'
-import { shallow } from 'enzyme';
+import { shallow } from 'enzyme'
 
+const CalendarWrapper = shallow(<Calendar/>)
+const testEvent = {
+  '1' : {
+    color: "green",
+    content: "Test text",
+    from: Date.now(),
+    to: Date.now()
+  }
+}
 
-it('Checks if ContactListItem component loads', () => {
-  shallow(<Calendar />)
-});
+test('Calendar component loads', () => {CalendarWrapper})
+
+test('createEvent adds the event to this.state', () => {
+  CalendarWrapper.instance().createEvent(testEvent)
+  expect(CalendarWrapper.instance().state.events).toEqual(testEvent)
+})
+
+test('createEvent adds the event to localStorage', () => {
+  CalendarWrapper.instance().createEvent(testEvent)
+  expect(localStorage.getItem('events')).toBe(JSON.stringify(testEvent))
+})
